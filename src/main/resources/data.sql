05ede79d74c639675739958e3669198d10e3d592
Gennadiy <relect@bk.ru>
2024-12-01 12:10:51 +0300

    add schema and data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@
     INSERT INTO book (title, author, publication_year)
     VALUES ('java', 'java-author', 2020),
            ('golang', 'golang-author', 2021),
-           ('python', 'python-author', 2019)
+           ('python', 'python-author', 2019);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 
     CREATE TABLE IF NOT EXISTS book (
-        id BIGINT AUTO_INCREMENT PRIMARY,
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
         title varchar(255),
         author varchar(255),
-        publication_year INT)
+        publication_year INT);
